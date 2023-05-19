72d2907fa3eae53b66939a448edaadbb0590816d
tyshkor <tyshko1@gmail.com>
2023-05-19 16:47:32 +0200

    consts

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
 use std::{collections::HashSet, ops::Range};
 
 use anyhow::{Error, Result};
+
+const FULL_RANGE: Range<usize> = 0..9;
+const RANGE: Range<usize> = 0..3;
+const SIZE: usize = 9;
+const ZERO: u8 = 0;
