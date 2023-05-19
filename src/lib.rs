63485c0f02e82adaa077ab05c5eed0da536dc3a2
tyshkor <tyshko1@gmail.com>
2023-05-19 16:47:52 +0200

    `Sudoku` struct

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,8 @@ const FULL_RANGE: Range<usize> = 0..9;
 const RANGE: Range<usize> = 0..3;
 const SIZE: usize = 9;
 const ZERO: u8 = 0;
+
+#[derive(Debug)]
+pub struct Sudoku {
+    grid: [[u8; SIZE]; SIZE],
+}
