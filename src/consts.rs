cc1634bfe8c84d68a67b787e64869e6672029c07
tyshkor <tyshko1@gmail.com>
2023-05-19 17:06:30 +0200

    consts added

---
+++ src/consts.rs
@@ -0,0 +1,6 @@
+use std::ops::Range;
+
+pub(crate) const FULL_RANGE: Range<usize> = 0..9;
+pub(crate) const RANGE: Range<usize> = 0..3;
+pub(crate) const SIZE: usize = 9;
+pub(crate) const ZERO: u8 = 0;
